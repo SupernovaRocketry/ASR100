f58bee018c17a13d3c15967774937b85aeee6f44
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-04-27 00:30:46 -0300

    Corrigindo o código de leitura

--- código/leituraAttiny/leituraAttiny.ino
+++ código/leituraAttiny/leituraAttiny.ino
@@ -1,10 +1,23 @@
-void setup() {
-Serial.begin(9600);
+#include<SoftwareSerial.h>
+#include<EEPROM.h>
+
+ SoftwareSerial bmpSerial(3, 4);
+ void setup() {
+ bmpSerial.begin(9600);
  }
 
-void loop() {
- if(Serial.available()){
- Serial.print((char) Serial.read());
- delay(10);
+ void loop() {
+ for(int addr=0; addr<EEPROM.length()-1; addr+=2){
+ bmpSerial.print(EEPROM.read(addr));
+ bmpSerial.print(',');
+ bmpSerial.println(EEPROM.read(addr+1));
+ delay(100);
+}
+ delay(5000);
+{
+if(bmpSerial.available()){
+ bmpSerial.print((char) bmpSerial.read());
+delay(10);
+ }
  }
  }
