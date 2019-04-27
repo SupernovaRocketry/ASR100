813bd450f71eaf9e1e55441e8453ca02699539a1
Gabriel Oliveira <gabriel.oliveira@engenharia.ufjf.br>
2019-04-27 00:33:10 -0300

    Adicionando identificadores de início e fim do eeprom dump

--- código/leituraAttiny/leituraAttiny.ino
+++ código/leituraAttiny/leituraAttiny.ino
@@ -7,12 +7,14 @@
  }
 
  void loop() {
+ bmpSerial.println("DUMP START");
  for(int addr=0; addr<EEPROM.length()-1; addr+=2){
  bmpSerial.print(EEPROM.read(addr));
  bmpSerial.print(',');
  bmpSerial.println(EEPROM.read(addr+1));
  delay(100);
 }
+ bmpSerial.println("DUMP END");
  delay(5000);
 {
 if(bmpSerial.available()){
